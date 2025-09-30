2673259c016869d825acac89f532e9c551495ec9
Evgenii <jonymaksimov697@gmail.com>
2025-09-30 18:06:50 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 const schemeSvg = document.querySelector('.scheme svg');
 const totalPriceTag = document.querySelector(".price-total");
-const menuButton = document.querySelector(".m-menu");
-const menu = document.querySelector(".menu");
 let cost = 800;
 let totalPrice = 0;
-
 schemeSvg.addEventListener("click", (event) => {
  if (!event.target.classList.contains("booked")) {
     event.target.classList.toggle("active");
@@ -13,7 +10,3 @@ schemeSvg.addEventListener("click", (event) => {
     totalPriceTag.textContent = totalPrice;
  }
 });
-menuButton.addEventListener("click", () => {
-   console.log("Кликнули по меню");
-   menu.classList.toggle("is-open");
-})
